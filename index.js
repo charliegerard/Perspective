5220edf753bcfff328657b3fec41c3ede6cb7044
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-03-07 09:47:14 +1100

    face tracking working with moving particles + clean code.

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ window.onload = function(){
     document.body.appendChild( container );
 
     scene = new THREE.Scene();
-    scene.fog = new THREE.Fog( 0x000000, 1, 5000 );
+    // scene.fog = new THREE.Fog( 0x000000, 1, 5000 );
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 );
     camera.position.z = 1000;
@@ -180,39 +180,6 @@ window.onload = function(){
     plane4.position.y = -250;
     plane4.position.z = 50-1500;
     scene.add( plane4 );
-
-    // Create sprites with lines
-
-    var placeTarget = function(x,y,z) {
-
-        // Cylinder
-        var cylinder = new THREE.Mesh( new THREE.CylinderGeometry(30,30,1,20,1,false), new THREE.MeshBasicMaterial( { color : 0xeeeeee} ) );
-        cylinder.position.x = x;
-        cylinder.rotation.x = Math.PI/2;
-        cylinder.position.y = y;
-        cylinder.position.z = z;
-        scene.add( cylinder );
-
-        var geometry = new THREE.Geometry();
-        geometry.vertices.push( new THREE.Vector3( 0, 0, -80000 ) );
-        geometry.vertices.push( new THREE.Vector3( 0, 0, z ) );
-        var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xeeeeee } ) );
-        line.position.x = x;
-        line.position.y = y;
-        scene.add( line );
-    }
-
-    // placeTarget(-150,-150,-550);
-    // placeTarget(0,-150,-200);
-    // placeTarget(100,0,500);
-    // placeTarget(-150,100,0);
-    // placeTarget(150,-100,-1050);
-    // placeTarget(50,0,1100);
-    // placeTarget(-50,-50,600);
-    // placeTarget(0,150,-2100);
-    // placeTarget(-130,0,-700);
-
-    //-------------------- END ----------------------
 
     var material = new THREE.SpriteMaterial({
       map: new THREE.CanvasTexture(generateSprite()),
